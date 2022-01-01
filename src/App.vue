0c5b02c633d5198fe26e7d41a78fd66c3356d2f1
srsohan150626 <sohan.ice.pust@gmail.com>
2022-01-01 12:54:59 +0600

    girt commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app" class="container">
+    <img class="logo" src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+.container{
+  max-width: 600px;
+  margin: 0 auto;
+}
+.logo{
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
